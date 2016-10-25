70894a83a534456f3a490d4b403b58a19177d51b
levelcode <leonardopalomino@MacBook-Pro-de-Leo.local>
2016-10-25 15:18:47 -0500

    Home Administrable y bases de los 5 templates

--- theme-alegria/includes/home.php
+++ theme-alegria/includes/home.php
@@ -3,56 +3,44 @@
     <div class="row">
         <div class="col-xs-12 hidden-xs frase">
             <div>
-                <p class="primera">movemos</p>
-                <img src="<?php bloginfo('template_url')?>/assets/corazon.png" alt="Alegria activity corazon">
-                <p class="segunda">emociones</p>
+                <p class="primera">Movemos Emociones</p>
             </div>
             
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-12 el1">
-            <div class="icono_hacemos"><img src="<?php bloginfo('template_url')?>/assets/icon_que_hacemos.png" alt="Que hacemos en alegria activty"></div>
-            <h1>¿qué hacemos?</h1>
-            <div class="ver_mas">ver más</div>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-lg-4 el2">
-            <div class="icono_alegria"><img src="<?php bloginfo('template_url')?>/assets/icono_alegria.png" alt="Alegria activity"></div>
-            <h1>100% alegría activity</h1>
-            <p>Producción de campañas</p>
-            <div class="ver_mas">ver más</div>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-lg-4 el3">
-            <div class="icono_casos"><img src="<?php bloginfo('template_url')?>/assets/icono_alegria.png" alt="Alegria activity Casos de exito"></div>
-            <h1>casos de éxito</h1>
-            <p>Conócen nuestros proyectos</p>
-            <div class="ver_mas">ver más</div>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-lg-4 el4">
-            <div class="icono_noticias"><img src="<?php bloginfo('template_url')?>/assets/icono_alegria.png" alt="Alegria activity Noticias"></div>
-            <h1>noticias</h1>
-            <p>Entérate de nuestras alegrias</p>
-            <div class="ver_mas">ver más</div>
-        </div>
-        <div class="col-xs-12 el5">
-            <div class="icono_mundo"><img src="<?php bloginfo('template_url')?>/assets/icono_alegria.png" alt="Alegria activity Noticias"></div>
-            <h1>alegría activity en el mundo</h1>
-            <p>Ponte en contácto con el resto de sedes</p>
-            <div class="ver_mas">ver más</div>
-        </div>
+        <?php 
+            $args = array(
+                'sort_order' => 'asc',
+                'sort_column' => 'menu_order',
+                'post_status' => 'publish'
+            );
+            $pages = get_pages($args); 
+            foreach ( $pages as $page ) {
+                $ancho = get_post_meta($page->ID, 'ancho' );
+                $fondo = get_post_meta($page->ID, 'fondo' );
+                $icono = get_post_meta($page->ID, 'icono' );
+                $subtitulo = get_post_meta($page->ID, 'subtitulo' );
+
+                $elemento = '<div class="'.$ancho[0].' '.$fondo[0].'">';
+                $elemento .= '<i class="fa '.$icono[0].'"></i>';
+                $elemento .= '<h1>'.$page->post_title.'</h1>';
+                $elemento .= '<p>'.$subtitulo[0].'</p>';
+                $elemento .= '<div class="ver_mas" data-link="'.get_page_link( $page->ID ).'">ver más</div>';
+                $elemento .= '</div>';
+                echo $elemento; 
+            }  
+        ?>
     </div>
 </div>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-xs-12 portafolio">
-            <img src="<?php bloginfo('template_url')?>/assets/icono_btn.png">
-            <h4>ver todos los</h4>
-            <h1>proyectos</h1>
         </div>
         <div class="col-xs-12 mapa">
             <div class="img_mapa"><img src="<?php bloginfo('template_url')?>/assets/mapa.png"></div>
-            <div class="paises">España <span class="separatas">-</span> Colombia <span class="separatas">-</span> Ecuador <span class="separatas">-</span> Chile <span class="separatas">-</span> Perú <span class="separatas">-</span> Dubai <span class="separatas">-</span> Marruecos <span class="separatas">-</span> Francia <span class="separatas">-</span> Angola <span class="separatas">-</span> Polska</div>
+            <div class="paises">España <span class="separatas">-</span> Colombia <span class="separatas">-</span> Ecuador <span class="separatas">-</span> Chile <span class="separatas">-</span> Perú <span class="separatas">-</span> Polonia <span class="separatas">-</span> Marruecos <span class="separatas">-</span> Francia <span class="separatas">-</span> Angola <span class="separatas">-</span> Polska</div>
         </div>
     </div>
 </div>
---
+++ theme-alegria/templates/noticias.php
@@ -0,0 +1,17 @@
+<?php
+	/*
+		Template name: Noticias
+	*/
+	get_header();
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12 portafolio">
+        </div>
+        <div class="col-xs-12 mapa">
+            <div class="img_mapa"><img src="<?php bloginfo('template_url')?>/assets/mapa.png"></div>
+            <div class="paises">España <span class="separatas">-</span> Colombia <span class="separatas">-</span> Ecuador <span class="separatas">-</span> Chile <span class="separatas">-</span> Perú <span class="separatas">-</span> Polonia <span class="separatas">-</span> Marruecos <span class="separatas">-</span> Francia <span class="separatas">-</span> Angola <span class="separatas">-</span> Polska</div>
+        </div>
+    </div>
+</div>
+<?php get_footer();?>
