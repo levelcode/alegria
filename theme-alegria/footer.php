2699a4de95190f36f05f56066092c1695607d8ad
levelcode <leonardopalomino@MacBook-Pro-de-Leo.local>
2016-11-17 10:15:49 -0500

    Footer

--- theme-alegria/footer.php
+++ theme-alegria/footer.php
@@ -2,9 +2,30 @@
 <footer>
 	<div class="container-fluid footer">
 	    <div class="row">
-			<img src="<?php bloginfo('template_url')?>/assets/logo_footer.png">
-			<div class="redes">	
-			</div>
+	    	<div class="col-xs-12">
+	    		<img src="<?php bloginfo('template_url')?>/assets/logo_footer.png">
+	    	</div>
+	    	<div class="col-xs-12 hidden-xs">
+	    		<?php
+                    wp_nav_menu( array(
+                        'menu'              => 'footer',
+                        'depth'             => 2,
+                        'container'         => 'div',
+                        'menu_class'        => 'nav navbar-nav',
+                        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                        'walker'            => new wp_bootstrap_navwalker())
+                    );
+                ?>
+	    	</div>
+	    	<div class="col-xs-12 social-footer">
+	    		<ul>
+	    			<li><a href="http://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="http://facebook.com/"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="http://linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+					<li><a href="http://plus.google.com/"><i class="fa fa-google-plus"></i> </a></li>
+					<li><a href="http://plus.google.com/"><i class="fa fa-youtube"></i> </a></li>
+				</ul>
+	    	</div>
 			<p>Todos los derechos reservados® Alegria Activity</p>
 		</div>
 	</div>
